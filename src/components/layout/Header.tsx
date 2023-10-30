e19499ce1d2c6f1f31d1e5ccb168c0f807dfe700
AlexMagar <lx.mgr07@gmail.com>
2023-10-30 22:35:21 +1100

    feat: added useMediaQuery and useTheme

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,19 +2,29 @@ import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { Button, Tab, Tabs } from '@mui/material';
+import { Button, Tab, Tabs, useMediaQuery, useTheme } from '@mui/material';
+import { DrawerComp } from './DrawerComp';
+
+// https://www.youtube.com/watch?v=lUkxSnJ7aDw
 
 export const Header = () => {
 
   const [value, setValue] = useState();
+  const theme = useTheme()
+  const isMatch = useMediaQuery(theme.breakpoints.down("md"))
 
   const handleOnChange = (e) => {
     const {name, value} = e.target;
+
+    setValue({
+      ...value,
+      [name]: value,
+    })
   }
 
   return (
     <React.Fragment>
-      <AppBar>
+      <AppBar sx={{background: "#063970"}}>
         <Toolbar>
           <Typography>LAX</Typography>
             <Tabs textColor='inherit' value={value} onChange={handleOnChange} indicatorColor='secondary'>
@@ -26,6 +36,7 @@ export const Header = () => {
             <Button sx={{marginLeft: "auto"}} variant='contained'>Login</Button>
             <Button sx={{marginLeft:"10px"}} variant='contained'>SingnUp</Button>
         </Toolbar>
+        <DrawerComp />
       </AppBar>
     </React.Fragment>
   )
