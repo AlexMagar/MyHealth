65c383df1986b2bc9594ed6eb4f9044cf5fb48ad
AlexMagar <lx.mgr07@gmail.com>
2023-11-01 22:31:41 +1100

    feat: added new section and passes args

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import { Header } from './components/layout/Header.tsx'
+import { Counter } from './components/layout/Counter.tsx'
 import { Footer } from './components/layout/Footer.tsx'
+import Header from './components/layout/Header.tsx'
+import { Section } from './components/layout/Section.tsx'
 
 function App() {
 
   return (
     <>
-    <Header/>
+    <Header title={'Hello Heading'}/>
     <h1>My Health</h1>
     <hr/>
+    <Section title='Hello Title'>
+      This is my Section
+    </Section>
+    <hr/>
+    <Counter/>
     <Footer/>
     </>
   )
