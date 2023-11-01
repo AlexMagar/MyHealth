9fa0fc17af263f87abbdfa389c8f086cc184e483
AlexMagar <lx.mgr07@gmail.com>
2023-11-01 22:32:49 +1100

    feat: returing prps via args

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,45 +1,11 @@
-import React, { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { Button, Tab, Tabs, useMediaQuery, useTheme } from '@mui/material';
-import { DrawerComp } from './DrawerComp';
+import { ReactElement } from 'react'
 
-// https://www.youtube.com/watch?v=lUkxSnJ7aDw
+type headingProps = {title: string}
 
-export const Header = () => {
-
-  const [value, setValue] = useState();
-  const theme = useTheme()
-  const isMatch = useMediaQuery(theme.breakpoints.down("md"))
-
-  const handleOnChange = (e) => {
-    const {name, value} = e.target;
-
-    setValue({
-      ...value,
-      [name]: value,
-    })
-  }
-
+const Header = ({title}:headingProps): ReactElement => {
   return (
-    <React.Fragment>
-      <AppBar sx={{background: "#063970"}}>
-        <Toolbar>
-          <Typography>LAX</Typography>
-            <Tabs textColor='inherit' value={value} onChange={handleOnChange} indicatorColor='secondary'>
-              <Tab label="Products"/>
-              <Tab label="services"/>
-              <Tab label="Contact"/>
-              <Tab label="About"/>
-            </Tabs>
-            <Button sx={{marginLeft: "auto"}} variant='contained'>Login</Button>
-            <Button sx={{marginLeft:"10px"}} variant='contained'>SingnUp</Button>
-        </Toolbar>
-        <DrawerComp />
-      </AppBar>
-    </React.Fragment>
+    <div>{title}</div>
   )
-}
+} 
 
-
+export default Header
