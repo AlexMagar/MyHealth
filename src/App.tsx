dc0f0f35d61ebea7735fda19ea9f3e3f85e6603c
AlexMagar <lx.mgr07@gmail.com>
2023-11-03 23:55:39 +1100

    feat: useEffect implemenation in TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Counter } from './components/layout/Counter.tsx'
 import { Footer } from './components/layout/Footer.tsx'
 import Header from './components/layout/Header.tsx'
 import { Section } from './components/layout/Section.tsx'
 import { List } from './components/layout/List.tsx'
+import { Count } from './components/layout/Count.tsx'
+
+interface User {
+  id: number,
+  username: string,
+}
 
 function App() {
 
   const [count, setCount] = useState<number>(1)
+  const [users, setUsers] = useState<User[] | null>(null)
+
+  useEffect(() => {
+    console.log('mounting')
+    console.log('Users: ', users)
+
+    return () => console.log('unmounting')
+  }, [users])
 
   return (
     <>
@@ -26,6 +40,8 @@ function App() {
       items={["string", 1223, "Boolean"]} 
       render={(item:string | number) => <span className='gold'>{item}</span>} 
     />
+    <hr/>
+    <Count setCount={setCount}>Count {count}</Count>
     <hr/>
     <Footer/>
     </>
