b9c9d0ebdcb02d11fcd392f41730492fc296df3b
AlexMagar <lx.mgr07@gmail.com>
2023-11-10 23:26:33 +1100

    feat: productList

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
+import useCart from '../hooks/useCart'
+import { useProducts } from '../hooks/useProducts'
+import { Product } from './layout/Product'
 
 export const ProductList = () => {
-  return (
-    <div>ProductList</div>
+
+  const {dispatch, REDUCER_ACTION, cart} = useCart()
+
+  const {products} = useProducts()
+
+  let pageContent: ReactElement | ReactElement[] = <p>Loading...</p>
+
+  if(products?.length){
+    const inCart: boolean = cart.some(item => item.sku === products.sku)
+
+    return (
+      <Product 
+          key={products.sku} 
+          products={products} 
+          dispatch={dispatch}
+          REDUCER_ACTION={REDUCER_ACTION}
+          inCart={inCart}
+      />
+    )
+  }
+
+  const content = (
+    <main className="main main--products">
+      {pageContent}
+    </main>
   )
+
+  return content
 }
