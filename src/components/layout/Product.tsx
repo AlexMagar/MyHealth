21aa6d5c767b67cb540f8b0d8f57e879b6fb2694
AlexMagar <lx.mgr07@gmail.com>
2023-11-12 21:31:30 +1100

    feat: define the content to display

--- src/components/layout/Product.tsx
+++ src/components/layout/Product.tsx
@@ -18,10 +18,18 @@ const Product = ({product, dispatch, REDUCER_ACTION, inCart}: PropsType): ReactE
     const img: string = new URL(`../../assets/${product.sku}.svg`, import.meta.url).href
 
     console.log(img)
-    const content = (
-        <>
 
-        </>
+    const onAddToCart = () => dispatch({type: REDUCER_ACTION.ADD, payload: {...product, qty: 1}})
+
+    const itemInput = inCart ? ' → Item in Cart: ✔️ ' : null
+
+    const content = (
+        <article className="product">
+            <h3>{product.name}</h3>
+            <img src={img} alt={product.name} className='product_img' />
+            <p>{new Intl.NumberFormat('en-UK', {style:'currency', currency: 'USD'}).format(product.price)}</p>
+            <button onClick={onAddToCart}>Add to cart</button>
+        </article>
     )
   return content
 }
