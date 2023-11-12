825243451429b1520e3227e73236c451d86d7884
AlexMagar <lx.mgr07@gmail.com>
2023-11-12 21:30:29 +1100

    feat: debug

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,7 +1,6 @@
 import React, { ReactElement } from 'react'
 import useCart from '../hooks/useCart'
 import { useProducts } from '../hooks/useProducts'
-import { Product } from './layout/Product'
 
 export const ProductList = () => {
 
@@ -11,18 +10,21 @@ export const ProductList = () => {
 
   let pageContent: ReactElement | ReactElement[] = <p>Loading...</p>
 
-  if(products?.length){
-    const inCart: boolean = cart.some(item => item.sku === products.sku)
 
-    return (
-      <Product 
-          key={products.sku} 
-          products={products} 
-          dispatch={dispatch}
-          REDUCER_ACTION={REDUCER_ACTION}
-          inCart={inCart}
-      />
-    )
+  if(products?.length){
+    pageContent = products.map(product => {
+      const inCart: boolean = cart.some(item => item.sku === products.sku)
+      return (
+        <Product 
+            key={products.sku} 
+            products={products} 
+            dispatch={dispatch}
+            REDUCER_ACTION={REDUCER_ACTION}
+            inCart={inCart}
+        />
+      )
+
+    })
   }
 
   const content = (
